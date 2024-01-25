b7f13302e17a519c3d0f88093854111698c5a9e0
Patrick Alves <patrickalvesarts@gmail.com>
2024-01-25 19:13:02 -0300

    Filmes e Favoritos

--- Films/TableViewController.swift
+++ Films/TableViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    var filmes = ["O Senhor Dos Aneis: O Retorno do Rei", "Matrix", "Jhon Wick", "Matrix", "Constantine", "Star Wars", "O Hobbit", "Gente Grande", "Se beber nao case", "De volta para o futuro", "joker", "Batman", "Homen de ferro", "Guardioes da galaxia", "Vingadores", "Loki",]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,23 +25,24 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return filmes.count
     }
 
-    /*
+   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
+        cell.textLabel!.text = filmes[indexPath.row]
         // Configure the cell...
 
         return cell
     }
-    */
+   
 
     /*
     // Override to support conditional editing of the table view.
