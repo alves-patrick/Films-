2d25374bc3f14c089dc92bf71b5b6c7fb2aa3387
Patrick Alves <patrickalvesarts@gmail.com>
2024-01-25 18:32:38 -0300

    Constrains iPhone/iPad, Alert fail login

--- Films/ViewController.swift
+++ Films/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var txtLogin: UITextField!
+    
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func openAction(_ sender: Any) {
+        
+        if (txtLogin.text == "patrick" &&
+            txtPassword.text == "1234"){
+            performSegue(withIdentifier: "tela2", sender: self)
+       
+        } else {
+            let alert = UIAlertController(title: "Alert", message: "Erro no Login/Senha", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
 }
+
 
