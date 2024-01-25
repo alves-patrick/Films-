7fa688a2420da30d6d34a6acec33b43410921df1
Patrick Alves <patrickalvesarts@gmail.com>
2024-01-25 17:30:26 -0300

    Initial Commit

---
+++ Films/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Films
+//
+//  Created by Patrick Alves on 1/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
